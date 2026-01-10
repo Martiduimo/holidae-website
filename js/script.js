2ea222bcd2b867d457170a5fb7717c4bd501d2de
Martiduimo <martiduimo@yahoo.com>
2026-01-10 16:49:20 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.getElementById("menuBtn");
+  const navMenu = document.getElementById("navMenu");
+
+  if (menuBtn && navMenu) {
+    menuBtn.addEventListener("click", () => {
+      navMenu.classList.toggle("hidden");
+    });
+  }
+});
